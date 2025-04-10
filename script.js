85baba97bd4608d745a274270696bb0320a68273
Jayeon Hwang <jayeon512@gmail.com>
2025-04-10 17:57:36 -0500

    add close account button with findIndex

--- script.js
+++ script.js
@@ -156,6 +156,23 @@ btnTransfer.addEventListener('click', function (e) {
 
 });
 
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log('Close');
+  if (inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin) {
+    const index = accounts.findIndex(acc => acc.username === currentAccount.username);
+
+    // delete account
+    accounts.splice(index, 1);
+
+    //Hide UI
+    containerApp.style.opacity = 0;
+
+    inputCloseUsername.value = inputClosePin.value = '';
+  }
+});
+
 
 // lecture
 
